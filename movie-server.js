a91cb7c30991cf03c90a1ea18847ccbd952e8d41
chaijewon <vcandjava@nate.com>
2020-03-22 09:57:20 +0900

    aaa1

--- movie-server.js
+++ movie-server.js
@@ -9,12 +9,32 @@ app.listen(port,()=>{
 })
 const Client=require("mongodb").MongoClient;
 
-app.get("/",(req,res)=>{
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+// real_data?page=1
+app.get("/real_data",(req,res)=>{
     /*var url="http://www.kobis.or.kr/kobis/business/main/searchMainDailyBoxOffice.do";
     request({url:url},function (err,request,json) {
         res.json(json);
 
     })*/
+    var page=req.query.page;
+    var rowSize=9;
+    var skip=(page*rowSize)-rowSize;
+    /*
+        page =1
+        0
+        page=2
+        9
+
+        ==> Router,Event => 데이터를 서버로 보내는 과정
+        ==> JQuery 연결
+        ==> 하위 클래스 => 이벤트 연결하는 방법
+
+     */
     var url="mongodb://203.224.133.121:27017";
     Client.connect(url,(err,client)=>{
         var db=client.db("mydb");
@@ -22,9 +42,30 @@ app.get("/",(req,res)=>{
         // SELECT * FROM movie WHERE mno=1
         // find({mno:1})
         // {} {} ==> [{},{}]
-        db.collection("movie").find({}).toArray(function(err,docs){
+        db.collection("movie").find({type:1}).skip(skip).limit(rowSize).toArray(function(err,docs){
             res.json(docs)
+            client.close();// 몽고디비 종료
         })
     })
 
 })
+// @RequestMapping("a.do")
+app.get("/movie_total",(req,res)=>{
+    // 몽고디비 연결
+    var url="mongodb://203.224.133.121:27017"
+    Client.connect(url,(err,client)=>{
+        var db=client.db("mydb");
+        db.collection("movie").find({type:1}).count(function(err,count){
+            res.json({totalpage:Math.ceil(count/9.0)})
+            client.close();
+            return count;
+        })
+    })
+})
+
+
+
+
+
+
+
--- src/movie_component/Released.js
+++ src/movie_component/Released.js
@@ -1,10 +1,60 @@
 import React,{Component} from 'react'
-
+import axios from 'axios'
+// 외부 데이터 읽기
 class Released extends Component{
+    // function Released(props)
+    constructor(props) {
+        super(props);
+
+        // 변수 선언  ==> useState() => 데이터를 한개만 지정
+        this.state={
+            movie_data:[],
+            page:1,
+            totalpage:0
+        }
+    }
+    /*
+           <App>
+
+           constructor() => componentWillMount() => render() => componentDidMount()
+                           ======================
+                                 삭제
+           constructor() => render() => componentDidMount()
+                                        ===================
+                                          setState() => componentDidUpdate()
+                                          render()
+     */
+    // function => useEffect
+    // function => useMemo , useCallback
+
+    componentDidMount() {
+        // 서버에서 데이터 읽기
+        axios.get('http://localhost:3355/real_data',{
+             params:{
+                 page:this.state.page
+             }
+        }).then((res)=>{
+             this.setState({movie_data:res.data})// render()재호출
+            // render() => 가상 돔 <==> 실제 돔
+            //             ====== 비교 diff ====== 다른 곳만 변경
+        })
+    }
+
     render(){
+        const html=this.state.movie_data.map((movie)=>
+            <div className={"col-md-4"}>
+                <div className="panel panel-primary">
+                    <div className="panel-heading">{movie.title}</div>
+                    <div className="panel-body">
+                        <img src={movie.poster} width={"100%"}/>
+                    </div>
+                </div>
+            </div>
+        )
         return(
-            <div>
-                <h1>현재상영영화</h1>
+            <div className={"row"}>
+                <h1 className={"text-center"}>현재상영영화</h1>
+                {html}
             </div>
         )
     }
